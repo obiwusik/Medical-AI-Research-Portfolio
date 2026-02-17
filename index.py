bb9ab41567fc0fb469e492f450d500c26143473a
obiwusik <obiwusik@gmail.com>
2026-02-17 17:01:16 +0100

    complete code for lung cancer classification and segmentation

---
+++ index.py
@@ -0,0 +1,66 @@
+from PIL import Image
+from imutils import paths
+import cv2
+
+
+# Using Pillow library
+# image_path= "Bengin case (1).jpg"
+# try:
+#     img=Image.open(image_path)
+    
+#     print(img.format, img.size, img.mode)
+    
+# except FileNotFoundError as e:
+#     print("Error : Image file not found: " , e)
+
+
+# General cases
+# Using imutils and cv2 library
+# imagePaths= list(paths.list_images("C:\Data-set"))
+# For printing the paths of the images
+# print(imagePaths)
+# for imagePath in imagePaths:
+#     image = cv2.imread(imagePath)
+#     cv2.imshow("Frame", image)
+#     cv2.waitKey()
+# cv2.destroyAllWindows()
+
+
+# # Bengin cases
+# imagePaths= list(paths.list_images("C:\Bengin cases"))
+# print(imagePaths)
+# for imagePath in imagePaths:
+#     image = cv2.imread(imagePath)
+#     cv2.imshow("Frame", image)
+#     cv2.waitKey()
+# cv2.destroyAllWindows()
+
+
+# Malignant cases
+# from imutils import paths
+# import cv2
+# imagePaths= list(paths.list_images("C:\Malignant cases"))
+# print(imagePaths)
+# for imagePath in imagePaths:
+#     image = cv2.imread(imagePath)
+#     cv2.imshow("Frame", image)
+#     cv2.waitKey()
+# cv2.destroyAllWindows()
+
+
+# Normal cases
+# imagePaths= list(paths.list_images("C:\Normal cases"))
+# print(imagePaths)
+# for imagePath in imagePaths:
+#     image = cv2.imread(imagePath)
+#     cv2.imshow("Frame", image)
+#     cv2.waitKey()
+# cv2.destroyAllWindows()
+
+
+
+# Pre-processing
+
+ 
+
+
